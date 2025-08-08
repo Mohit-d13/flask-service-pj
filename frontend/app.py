6369984b6e8e6a6fd699c1b3743bc77988ba3a7d
Mohit-d13 <mohitdamle14@gmail.com>
2025-08-08 22:06:33 +0000

    resting commits

--- frontend/app.py
+++ frontend/app.py
@@ -57,6 +57,10 @@ def to_do_item():
         error = None
         if not item_dt['id']:
             error = "Item id is required."
+        if not item_dt['uuid']:
+            error = "UUID is missing"
+        if not item_dt['item_hash']:
+            error = "Item hash is missing"
         if not item_dt['name']:
             error = "Item name is required."
         if not item_dt['description']:
